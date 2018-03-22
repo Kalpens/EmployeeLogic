39dd1c404dde86a4a14369e44419a9280f283353
Mareks Kalējs <M.Kalejs@outlook.com>
2018-03-22 18:50:40 +0100

    Initial commit

---
+++ EmployeeLogic/EmployeeLogic.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EmployeeSorter
+{
+    public class EmployeeLogic : IEmployeeLogic
+    {
+        public void SetEmployees(List<Employee> employees)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetManagers()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public Employee GetManagerByDepartment(int departmentId)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public Employee GetManagerByEmployeeId(int employeeSSN)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetSortedEmployeesBySSN()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetSortedEmployeesByName()
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+
+        public List<Employee> GetEmployeesByDepartment(int departmentId)
+        {
+            //TODO
+            throw new NotImplementedException();
+        }
+    }
+}
