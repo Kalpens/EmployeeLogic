741c4b90a1c0f362b22b318e73894402d086c087
Mareks Kalējs <M.Kalejs@outlook.com>
2018-04-11 09:16:26 +0200

    Completed by sub-contractor

--- EmployeeSorter/EmployeeLogic.cs
+++ EmployeeSorter/EmployeeLogic.cs
@@ -1,51 +1,65 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace EmployeeSorter
 {
     public class EmployeeLogic : IEmployeeLogic
     {
+        private List<Employee> employees = new List<Employee>();
         public void SetEmployees(List<Employee> employees)
         {
-            //TODO
-            throw new NotImplementedException();
+            this.employees = employees;
         }
 
         public List<Employee> GetManagers()
         {
-            //TODO
-            throw new NotImplementedException();
+            List<Employee> managers = new List<Employee>();
+
+            foreach (var employee in employees)
+            {
+                if (employee.SuperSSN == 0)
+                {
+                    List<Employee> slaves = employees.FindAll(x => x.SuperSSN == employee.SSN);
+                    if (slaves != null && slaves.Count > 0)
+                        managers.Add(employee);
+                }
+            }
+            return managers;
         }
 
         public Employee GetManagerByDepartment(int departmentId)
         {
-            //TODO
-            throw new NotImplementedException();
+            Employee mgr = employees.Find(x => x.DepartmentId == departmentId && x.SuperSSN == 0);
+            if (mgr == null)
+                mgr = new Employee();
+            return mgr;
+            //Suggestion: Maybe use nullable ints when working with SSN?
         }
 
         public Employee GetManagerByEmployeeId(int employeeSSN)
         {
-            //TODO
-            throw new NotImplementedException();
+            Employee emp = employees.Find(y => y.SSN == employeeSSN);
+            Employee mgr = employees.Find(x => x.SSN == emp.SuperSSN);
+            if (mgr == null)
+                mgr = new Employee();
+            return mgr;
         }
 
         public List<Employee> GetSortedEmployeesBySSN()
         {
-            //TODO
-            throw new NotImplementedException();
+                return employees.OrderBy(x => x.SSN).ToList();
         }
 
         public List<Employee> GetSortedEmployeesByName()
         {
-            //TODO
-            throw new NotImplementedException();
+            return employees.OrderBy(x => x.Name).ToList();
         }
 
         public List<Employee> GetEmployeesByDepartment(int departmentId)
         {
-            //TODO
-            throw new NotImplementedException();
+            return employees.FindAll(x => x.DepartmentId == departmentId);
         }
     }
 }
